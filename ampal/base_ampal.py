4564d0e584ac8da9076d5113e0cc515088378390
Chris Wells Wood <cwwoodesq@gmail.com>
2016-10-10 17:27:37 +0100

    Updated to use ff index instead of adding params, currently is not used in the actual scoring.

--- ampal/base_ampal.py
+++ ampal/base_ampal.py
@@ -135,6 +135,7 @@ class BaseAmpal(object):
         raise NotImplementedError
 
     def assign_force_field(self, ff, mol2=False):
+        parameter_key = ff.parameter_key
         if hasattr(self, 'ligands'):
             atoms = self.get_atoms(ligands=True, inc_alt_states=True)
         else:
@@ -143,12 +144,11 @@ class BaseAmpal(object):
             if atom.element == 'H':
                 continue
             elif atom.ampal_parent.mol_code in ff:
-                if atom.res_label in ff['WLD']:
-                    a_ff = ff['WLD'][atom.res_label]
-                else:
-                    a_ff = ff[atom.ampal_parent.mol_code][atom.res_label]
+                a_ff_index = parameter_key[(atom.ampal_parent.mol_code, atom.res_label)]
+            elif atom.res_label in ff['WLD']:
+                a_ff_index = parameter_key[('WLD', atom.res_label)]
             elif mol2 and (atom.ampal_parent.mol_code.capitalize() in ff['MOL2']):
-                a_ff = ff['MOL2'][atom.res_label.capitalize()]
+                a_ff_index = parameter_key[('MOL2', atom.res_label.capitalize())]
             else:
                 if not mol2:
                     w_str = '{} ({}) atom is not parameterised in the selected residue force field. ' \
@@ -158,7 +158,7 @@ class BaseAmpal(object):
                                                                                                  atom.res_label)
                 warnings.warn(w_str, NotParameterisedWarning)
                 continue
-            atom.tags['ff_params'] = PyAtomData(atom.res_label.encode(), a_ff[0].encode(), *a_ff[1:])
+            atom._ff_index = a_ff_index
         self.tags['assigned_ff'] = True
         return
 
@@ -485,6 +485,7 @@ class Atom(object):
             'charge': charge,
             'state': state
         }
+        self._ff_index = None
 
     def __repr__(self):
         return "<{} Atom{}. Coordinates: ({:.3f}, {:.3f}, {:.3f})>".format(
