a192ba265c4f152654b15c8a602fafd9ff7123a6
Chris Wells Wood <cwwoodesq@gmail.com>
2016-11-17 16:13:04 +0000

    Added tests for packing models of the basis set.

--- unit_tests/test_external_programs.py
+++ unit_tests/test_external_programs.py
@@ -6,9 +6,13 @@ from hypothesis.strategies import text
 import isambard_dev as isambard
 
 
+cannonical_labels = 'ACDEFGHIKLMNPQRSTVWY'
+non_cannonical_labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
 class TestScwrl4(unittest.TestCase):
 
-    @given(text('ACDEFGHIKLMNPQRSTVWY', min_size=5, max_size=5))
+    @given(text(cannonical_labels, min_size=5, max_size=5))
     @settings(max_examples=20)
     def test_helix_seq_packing(self, sequence):
         """Test Scwrl sidechain packing using a small helix."""
@@ -16,10 +20,10 @@ class TestScwrl4(unittest.TestCase):
         helix.pack_new_sequence(sequence)
         self.assertEqual(helix.sequence, sequence)
 
-    @given(text('ABCDEFGHIJKLMNOPQRSTUVWXYZ', min_size=5, max_size=5))
+    @given(text(non_cannonical_labels, min_size=5, max_size=5))
     @settings(max_examples=20)
     def test_helix_non_canonical(self, sequence):
-        """Test Scwrl sidechain packing using a small helix."""
+        """Test Scwrl sidechain packing when given non-cannonical residue labels."""
         helix = isambard.specifications.Helix(5)
         helix.pack_new_sequence(sequence)
         r_seq = sequence[:]
@@ -30,3 +34,36 @@ class TestScwrl4(unittest.TestCase):
             self.assertEqual(helix.sequence, r_seq)
         else:
             self.assertEqual(helix.sequence, r_seq)
+
+    @given(text(non_cannonical_labels + non_cannonical_labels.lower(), min_size=5, max_size=5))
+    @settings(max_examples=20)
+    def test_helix_non_canonical_lower(self, sequence):
+        """Test Scwrl sidechain packing using upper and lower case labels."""
+        helix = isambard.specifications.Helix(5)
+        helix.pack_new_sequence(sequence)
+        r_seq = sequence[:]
+        non_can = [x for x in sequence if x.upper() not in isambard.tools.amino_acids.standard_amino_acids.keys()]
+        if non_can:
+            for aa in non_can:
+                r_seq = r_seq.replace(aa, 'G')
+            self.assertEqual(helix.sequence, r_seq.upper())
+        else:
+            self.assertEqual(helix.sequence, r_seq.upper())
+
+    def test_basis_set_dimer(self):
+        """Test packing basis set dimer model."""
+        cc_di = isambard.specifications.CoiledCoil(2)
+        cc_di.pack_new_sequences(cc_di.basis_set_sequences)
+        self.assertEqual(cc_di.sequences, cc_di.basis_set_sequences)
+
+    def test_basis_set_trimer(self):
+        """Test packing basis set trimer model."""
+        cc_tri = isambard.specifications.CoiledCoil(3)
+        cc_tri.pack_new_sequences(cc_tri.basis_set_sequences)
+        self.assertEqual(cc_tri.sequences, cc_tri.basis_set_sequences)
+
+    def test_basis_set_tetramer(self):
+        """Test packing basis set tetramer model."""
+        cc_tet = isambard.specifications.CoiledCoil(4)
+        cc_tet.pack_new_sequences(cc_tet.basis_set_sequences)
+        self.assertEqual(cc_tet.sequences, cc_tet.basis_set_sequences)
