34878590d4d3f62837acad1310cd5b4358a302e9
Chris Wells Wood <cwwoodesq@gmail.com>
2017-06-21 10:25:11 +0100

    Removed settings.json and references.

--- isambard/settings.py
+++ isambard/settings.py
@@ -1,8 +1,8 @@
 """Loads a json file containing settings that the ISAMBARD use.
 
-A settings.json file must be made in this directory, template_settings.py can be copied and paths for your system
+A .isambard_settings file must be made in this directory, template_settings.py can be copied and paths for your system
 can be added. Json files look like python dictionaries, and in fact all json files are valid python dictionary code
-(although this isn't necessarily true in reverse). The settings.json file is ignored by git to it avoid being
+(although this isn't necessarily true in reverse). The .isambard_settings file is ignored by git to it avoid being
 overwritten with another systems paths. When the isambard is imported a isambard.settings dictionary is generated, which
 contains the user chosen settings, but it can be explicitly loaded by an internal module to get these values too.
 """
