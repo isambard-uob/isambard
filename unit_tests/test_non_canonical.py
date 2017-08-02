c47f06bf8143fafe394dd80f00f1f2baa0cf2665
Chris Wells Wood <cwwoodesq@gmail.com>
2017-08-02 11:12:40 +0100

    Adds test for maintaining common atom positions.

--- unit_tests/test_non_canonical.py
+++ unit_tests/test_non_canonical.py
@@ -1,3 +1,4 @@
+import copy
 import unittest
 
 import isambard_dev as isambard
@@ -14,12 +15,17 @@ class TestConvertProToHyp(unittest.TestCase):
         to_convert = [
             res for (i, res) in enumerate(col.get_monomers())
             if not (i + 1) % 3]
+        ori_pros = copy.deepcopy(to_convert)
         for pro in to_convert:
             isambard.ampal.non_canonical.convert_pro_to_hyp(pro)
         self.assertEqual(col.sequences, ['GPXGPXGPXGPXGPXGPXGPX'] * 3)
-        hyps = list(filter(lambda x: x.mol_code == 'HYP', col.get_monomers()))
+        hyps = to_convert
         self.assertEqual(len(hyps), 7 * 3)
         hyp_atom_labels = ('N', 'CA', 'C', 'O', 'CB', 'CG', 'CD', 'OD1')
-        for hyp in hyps:
-            self.assertTrue(
-                all(map(lambda x: x in hyp_atom_labels, hyp.atoms.keys())))
+        common_atoms = ('N', 'CA', 'C', 'O', 'CB')
+        for (pro, hyp) in zip(ori_pros, hyps):
+            for (label, atom) in hyp.atoms.items():
+                self.assertTrue(label in hyp_atom_labels)
+                if label in common_atoms:
+                    self.assertTrue(
+                        numpy.allclose(atom.array, hyp[label].array))
