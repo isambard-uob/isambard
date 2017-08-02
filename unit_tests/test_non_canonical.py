2cbf9e29f94b4ff098a16605d59759d28d12ad0f
Chris Wells Wood <cwwoodesq@gmail.com>
2017-08-02 10:45:51 +0100

    Adds unittest module for non_canonical.py

---
+++ unit_tests/test_non_canonical.py
@@ -0,0 +1,25 @@
+import unittest
+
+import isambard_dev as isambard
+import numpy
+
+
+@unittest.skipUnless('scwrl' in isambard.settings.global_settings,
+                     "External program not detected.")
+class TestConvertProToHyp(unittest.TestCase):
+
+    def test_convert_pro_to_hyp(self):
+        col = isambard.specifications.CoiledCoil.tropocollagen(aa=21)
+        col.pack_new_sequences(['GPPGPPGPPGPPGPPGPPGPP'] * 3)
+        to_convert = [
+            res for (i, res) in enumerate(col.get_monomers())
+            if not ((i + 1) % 3)]
+        for pro in to_convert:
+            isambard.ampal.non_canonical.convert_pro_to_hyp(pro)
+        self.assertEqual(col.sequences, ['GPXGPXGPXGPXGPXGPXGPX'] * 3)
+        hyps = list(filter(lambda x: x.mol_code == 'HYP', col.get_monomers()))
+        self.assertEqual(len(hyps), 7 * 3)
+        hyp_atom_labels = ('N', 'CA', 'C', 'O', 'CB', 'CG', 'CD', 'OD1')
+        for hyp in hyps:
+            self.assertTrue(
+                all(map(lambda x: x in hyp_atom_labels, hyp.atoms.keys())))
