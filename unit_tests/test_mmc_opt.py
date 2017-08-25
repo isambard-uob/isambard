717b297937abcc72a16d9ba780fe52b772d93bee
Chris Wells Wood <cwwoodesq@gmail.com>
2017-08-25 10:10:04 +0100

    Adds tests for check_move in MMC optimiser.

--- unit_tests/test_mmc_opt.py
+++ unit_tests/test_mmc_opt.py
@@ -4,8 +4,9 @@ import unittest
 
 from hypothesis import given
 from hypothesis.strategies import floats
+import isambard_dev as isambard
 from isambard_dev.optimisation.mmc_optimizer import (
-    MMCParameter, MMCParameterType)
+    MMCParameter, MMCParameterType, MMCParameterOptimisation)
 
 
 class TestMMCParameter(unittest.TestCase):
@@ -59,3 +60,36 @@ class TestMMCParameter(unittest.TestCase):
         self.assertTrue(uniform.current_value == cur_val)
         self.assertTrue(uniform.current_value != prop_val)
         self.assertTrue(uniform.proposed_value is None)
+
+    def test_build_from_parameters(self):
+        """Tests model building using parameters."""
+        input_parameters = (
+            MMCParameter('Oligomeric State', MMCParameterType.STATIC_VALUE, 2),
+            MMCParameter('Amino Acids', MMCParameterType.STATIC_VALUE, 28),
+            MMCParameter('Radius', MMCParameterType.UNIFORM_DIST, (2, 10)),
+            MMCParameter('Pitch', MMCParameterType.UNIFORM_DIST, (50, 300)),
+            MMCParameter('PhiCA', MMCParameterType.UNIFORM_DIST, (150, 360)),
+        )
+        build_args = [p.current_value for p in input_parameters]
+        coco = isambard.specifications.CoiledCoil.from_parameters(*build_args)
+        self.assertEqual(len(coco), 2)
+        self.assertEqual([len(x) for x in coco], [28, 28])
+        self.assertTrue(all([2 <= rad <= 10 for rad in coco.major_radii]))
+        self.assertTrue(all([50 <= rad <= 300 for rad in coco.major_pitches]))
+        self.assertTrue(all([150 <= rad <= 360 for rad in coco.phi_c_alphas]))
+
+
+class TestMMCOptimizer(unittest.TestCase):
+    """Tests functionality in the MMC optimizer."""
+    @given(floats(allow_nan=False, allow_infinity=False),
+           floats(allow_nan=False, allow_infinity=False),
+           floats(allow_nan=False, allow_infinity=False))
+    def test_check_move(self, old, new, temp):
+        """Checks a random move."""
+        MMCParameterOptimisation.check_move(old, new, temp)
+
+    @given(floats(allow_nan=False, allow_infinity=False),
+           floats(allow_nan=False, allow_infinity=False))
+    def test_check_move_frozen(self, old, new):
+        """Checks a move at 0 K, which should always be false."""
+        self.assertFalse(MMCParameterOptimisation.check_move(old, new, 0))
