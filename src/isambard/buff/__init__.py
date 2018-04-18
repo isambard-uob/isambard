2bbda73124818673989206137bbbdb6ed2dfcf51
Chris Wells Wood <cwwoodesq@gmail.com>
2018-04-18 09:54:22 +0100

    Structures with project and simplifies content.

--- src/isambard/buff/__init__.py
+++
@@ -1,3 +0,0 @@
-from .force_field import BuffForceField, force_fields
-from .calculate_energy import PyAtomData, score_interactions, get_within_ff_cutoff, find_inter_ampal, \
-    find_intra_ampal
--- src/isambard/external_programs/__init__.py
+++
@@ -1,5 +0,0 @@
-from .dssp import run_dssp, extract_all_ss_dssp, extract_helices_dssp, extract_pp_helices,\
-    find_ss_regions
-from .scwrl import run_scwrl, pack_sidechains, parse_scwrl_out
-from .profit import run_profit
-from .reduce import assembly_plus_protons
