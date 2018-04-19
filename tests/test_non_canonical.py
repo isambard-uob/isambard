edbb4fc01dba52b4f147a852a583b2b223a9f388
Chris Wells Wood <cwwoodesq@gmail.com>
2018-04-19 15:09:54 +0100

    Updates tests to match new structure.

--- tests/test_non_canonical.py
+++ tests/test_non_canonical.py
@@ -4,22 +4,23 @@ import copy
 import unittest
 
 import isambard
+import isambard.specifications as specs
+from isambard.modelling import convert_pro_to_hyp
+from isambard.modelling.scwrl import scwrl_available, pack_side_chains_scwrl
 import numpy
 
 
-@unittest.skipUnless(
-    'scwrl' in isambard.settings.global_settings,
-    "External program not detected.")
+@unittest.skipUnless(scwrl_available(), "External program not detected.")
 class TestConvertProToHyp(unittest.TestCase):
     def test_convert_pro_to_hyp(self):
-        col = isambard.specifications.CoiledCoil.tropocollagen(aa=21)
-        col.pack_new_sequences(['GPPGPPGPPGPPGPPGPPGPP'] * 3)
+        col = specs.CoiledCoil.tropocollagen(aa=21)
+        col = pack_side_chains_scwrl(col, ['GPPGPPGPPGPPGPPGPPGPP'] * 3)
         to_convert = [
             res for (i, res) in enumerate(col.get_monomers())
             if not (i + 1) % 3]
         ori_pros = copy.deepcopy(to_convert)
         for pro in to_convert:
-            isambard.ampal.non_canonical.convert_pro_to_hyp(pro)
+            convert_pro_to_hyp(pro)
         self.assertEqual(col.sequences, ['GPXGPXGPXGPXGPXGPXGPX'] * 3)
         hyps = to_convert
         self.assertEqual(len(hyps), 7 * 3)
