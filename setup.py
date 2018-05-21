f6ebcf26caa8d04f40805b15c22dc516a0b78751
Chris Wells Wood <cwwoodesq@gmail.com>
2018-05-21 14:03:53 +0100

    Updates version number.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 
 setup(name='ISAMBARD',
-      version='2.0.0',
+      version='2.0.1',
       description='A package for biomolecular analysis, modelling and design',
       long_description=readme(),
       long_description_content_type='text/markdown; charset=UTF-8; variant=GFM',
