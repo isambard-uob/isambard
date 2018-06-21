d9b8ad69828dccd8331896335789907f80553c5e
Ajasja Ljubetic <ajasja.ljubetic@gmail.com>
2018-06-21 16:15:58 +0200

    Fixed pacc functionality for antiparallel CCs as well.
    Also added/fixed unit tests.

--- tests/test_pacc.py
+++ tests/test_pacc.py
@@ -3,15 +3,16 @@ import os
 import unittest
 
 import isambard
+import ampal
 import isambard.evaluation.pacc as pacc
-
+import numpy as np
 
 class PACCTestCase(unittest.TestCase):
     """Tests for isambard.evaluation.pacc"""
 
     def setUp(self):
-        test_cc_folder = os.path.join(os.path.dirname(isambard.__file__), 'unit_tests', 'testing_files', 'test_ccs')
-        self.pdb_paths = glob.glob('{}/*'.format(test_cc_folder))
+        self.test_cc_folder = os.path.join(os.path.dirname(__file__), 'testing_files', 'test_pacc')
+        self.pdb_paths = glob.glob('{}/*'.format(self.test_cc_folder))
 
     def check_register(self, heptad_register, sequence_len):
         self.assertEqual(len(heptad_register), sequence_len)
@@ -23,9 +24,10 @@ class PACCTestCase(unittest.TestCase):
 
     def test_pacc_on_ccs(self):
         """Tests PACC for each file in testing_files/test_ccs"""
+        assert len(self.pdb_paths) > 0
         for file in self.pdb_paths:
-            cc = isambard.ampal.load_pdb(file)
-            ccr = isambard.ampal.Assembly()
+            cc = ampal.load_pdb(file)
+            ccr = ampal.Assembly()
             for h in cc:
                 ccr.append(h[1:28])
             cc_pacc = pacc.PACCAnalysis(ccr)
@@ -35,3 +37,16 @@ class PACCTestCase(unittest.TestCase):
             for p in [cc_pacc.radii_layers, cc_pacc.alpha_layers, cc_pacc.ca_layers]:
                 self.check_parameters(cc_pacc.calc_average_parameters(p)[0], cc_pacc.cc_len)
         return
+
+    def test_pacc_parallel_antiparallel(self):
+        file_name = os.path.join(self.test_cc_folder, 'p3_p4.pdb')
+        cc = ampal.load_pdb(file_name)
+        cca = pacc.PACCAnalysis(cc)
+        radius = np.mean(cca.radii_layers)
+        assert radius < 5.1
+
+        file_name = os.path.join(self.test_cc_folder, 'APH.pdb')
+        cc = ampal.load_pdb(file_name)
+        cca = pacc.PACCAnalysis(cc)
+        radius = np.mean(cca.radii_layers)
+        assert radius < 5.1
